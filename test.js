d495a9e65777e0652c65720fd1e11afc33d47725
John Lyons <johnlyonsdev@gmail.com>
2021-10-25 15:45:07 -0500

    Added second test

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('Second greeting test', () => {
+  expect(getGreeting('Bob')).toBe("Hi Bob");
+})
